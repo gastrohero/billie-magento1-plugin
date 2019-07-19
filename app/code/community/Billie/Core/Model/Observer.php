639980ebd0a12ba7ba9f8c4f8420cd1d69a186be
Ralf Siepker <ralf.siepker@mageconsult.de>
2019-07-19 16:21:56 +0200

    fix path for autoloader

--- app/code/community/Billie/Core/Model/Observer.php
+++ app/code/community/Billie/Core/Model/Observer.php
@@ -5,7 +5,7 @@ class Billie_Core_Model_Observer
     const AUTOLOADER_FILE = 'lib/Billie/vendor/autoload.php';
     public function addAutoloader()
     {
-        require_once self::AUTOLOADER_FILE;
+        require_once Mage::getBaseDir('base') . DS . self::AUTOLOADER_FILE;
         return $this;
     }
 }
